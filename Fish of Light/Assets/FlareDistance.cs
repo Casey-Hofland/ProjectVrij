bd66bb793797e8c6092e8615f1ad85ceb1b0bf97
Casey Hofland <hofland.casey@gmail.com>
2019-06-15 16:48:16 +0200

    Casey: Sonar Light Implemented

---
+++ Fish of Light/Assets/FlareDistance.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FlareDistance : MonoBehaviour
+{
+	[SerializeField] private StudioEventEmitter studioEvent;
+	[SerializeField] private Transform target;
+
+	void Update()
+    {
+		float distance = Vector3.Distance(transform.position, target.position);
+		float distPercentage = Mathf.Clamp01((studioEvent.OverrideMaxDistance - distance) / studioEvent.OverrideMaxDistance);
+		studioEvent.SetParameter("distanceToPlayer", distPercentage);
+    }
+}
