177cd44cf302d076602fb55ae77021daebfb0b5b
Julius de Blaaij <julius.de.blaaij@gmail.com>
2019-06-21 13:35:39 +0200

    Julius: Added WIP tutorial text in Intro

---
+++ Fish of Light/Assets/_Project/Code/FadeTMProText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FadeTMProText : MonoBehaviour
+{
+    TextMeshProUGUI textmeshPro;
+    Color32 startColor;
+    byte startAlpha;
+    byte currentAlpha;
+
+    void Awake()
+    {
+        textmeshPro = GetComponent<TextMeshProUGUI>();
+        startAlpha = textmeshPro.faceColor.a;
+    }
+
+    public void Fade()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        for (currentAlpha = startAlpha; currentAlpha > 0; currentAlpha--)
+        {
+            Color32 _color = startColor;
+            _color.a = currentAlpha;
+            textmeshPro.faceColor = _color;
+            Debug.Log("Tutorial alpha: " + currentAlpha);
+            
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
